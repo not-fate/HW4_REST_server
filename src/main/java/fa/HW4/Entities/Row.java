ba793b4b4f404fe6f1e620dda4e5a52e8ce84efd
Nadezhda <avarice2205@gmail.com>
2023-12-21 15:03:45 +0100

    added annotations

--- src/main/java/fa/HW4/Entities/Row.java
+++ src/main/java/fa/HW4/Entities/Row.java
@@ -1,22 +1,17 @@
 package fa.HW4.Entities;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-/**
- * <h3> Сущность записи из таблицы TREES.</h3>
- */
 @Entity
 @Table(name="trees")
 public class Row {
-    /**
-     * Уникальный идентификатор узла.
-     */
     @Id
-    public int id;
-    /**
-     * Уникальный идентификатор родительского узла.
-     */
-    public int parent_id;
+    @Column(name = "id")
+    public int ID;
+
+    @Column(name = "parent_id")
+    public int parentID;
 }
